ecce868979c6204f8fb8508f6a8a9833be69acc5
DribDrob <drobushenko@gmail.com>
2022-05-07 21:57:17 +0300

    Task1 Color Switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+    startBtnEl: document.querySelector('[data-start]'),
+    stopBtnEl: document.querySelector('[data-stop]'),
+    bodyEl: document.querySelector('body'),
+}
+let intervalId = null;
 
+refs.stopBtnEl.setAttribute('disabled', 'disabled');
+refs.startBtnEl.addEventListener('click', onStartBtnClick);
+refs.stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick (){
+    intervalId = setInterval(()=>refs.bodyEl.style.backgroundColor = getRandomHexColor(), 1000)
+    refs.startBtnEl.setAttribute('disabled', 'disabled');
+    refs.stopBtnEl.removeAttribute('disabled');
+}
+function onStopBtnClick (){
+    clearInterval( intervalId)
+    refs.startBtnEl.removeAttribute('disabled');
+    refs.stopBtnEl.setAttribute('disabled', 'disabled');
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
