9881e4028d16ca72d965200fe4bcb0a6fcaac110
DribDrob <drobushenko@gmail.com>
2022-05-11 12:45:36 +0300

    Task 3 Promises

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -30,36 +30,32 @@ const options = {
   function onStartBtnClick (){
     startBtnEl.setAttribute('disabled', 'disabled');
     dateInput.setAttribute('disabled', 'disabled');
-
-    const currentDate = Date.now();
+    
     const selectedDate = new Date(dateInput.value).getTime(); 
-    let diffTime = selectedDate - currentDate;
+    
    intervalId = setInterval(()=> {
+    const currentDate = Date.now();
+    let diffTime = selectedDate - currentDate;
     days.textContent = `${addLeadingZero(convertMs(diffTime).days)}`;
     hours.textContent = `${addLeadingZero(convertMs(diffTime).hours)}`;
     minutes.textContent = `${addLeadingZero(convertMs(diffTime).minutes)}`;
     seconds.textContent = `${addLeadingZero(convertMs(diffTime).seconds)}`;
-    diffTime -= 1000;
-    if (diffTime<0){
+    console.log(diffTime);
+    if (diffTime<999){
       clearInterval(intervalId);
     }
   } , 1000)
   }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -67,9 +63,3 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
-// Библиотека уведомлений
-// ВНИМАНИЕ
-// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
-
-// Для отображения уведомлений пользователю вместо window.alert() используй библиотеку notiflix.
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit (event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const step = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
+  let delay = Number(formElements.delay.value);
+ 
+  for (let i = 1; i <= amount; i += 1){
+    createPromise(i, delay).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {useIcon: false});
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {useIcon: false});
+        });
+        delay+=step;  
   }
-}
+ }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject)=>{
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+    resolve({position: position, delay: delay});
+  } 
+    else {reject({position: position, delay: delay});}
+  }, delay);
+})}
+
+
